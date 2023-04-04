d98dce446d522052bb370c4cdd4a35a851a7bdc4
jasserhussain <jasserhussain@gmail.com>
2023-04-05 01:01:49 +0530

    initial commit

---
+++ src/main/java/test/TestSubstring.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestSubstring {
+	public TestSubstring() {
+		System.out.println("test new java constructor");
+	}
+	public static String getSubString(String s) {
+		return s.substring(6);
+	}
+}
